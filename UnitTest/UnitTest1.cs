c32d2c317330e17248b5ecba1b33f1ee37c34315
danielsbonnin <bonnind@oregonstate.edu>
2016-01-13 19:45:50 -0600

    Added unit test for scoreGame()

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,7 +5,7 @@ using Checkers;
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TestCheckersGame
     {
         [TestMethod]
         public void test_initGame() 
@@ -14,5 +14,25 @@ namespace UnitTest
             Assert.IsTrue(game.squares[0].squareType == CheckerType.whiteChecker);
             
         }
+        [TestMethod]
+        public void test_scoreGame()
+        {
+            CheckersGame game = new CheckersGame();
+            //Start with a blank board
+            for (int i = 0; i < 32; i++)
+                game.squares[i].squareType = CheckerType.empty;
+            game.squares[4].squareType = CheckerType.whiteKing;
+            game.squares[0].squareType = CheckerType.whiteChecker;
+            game.squares[9].squareType = CheckerType.whiteChecker;
+            game.squares[14].squareType = CheckerType.whiteChecker;
+            game.squares[10].squareType = CheckerType.redChecker;
+            game.squares[29].squareType = CheckerType.redChecker;
+            game.squares[17].squareType = CheckerType.redKing;
+            game.squares[16].squareType = CheckerType.redKing;
+            double score = game.scoreGame(game, false);
+            double epsilon = 0.001;
+            Assert.IsTrue(epsilon > Math.Abs(score - 0.583333));
+        }
+        
     }
 }
